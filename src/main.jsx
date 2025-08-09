6da5c918eb98ec12422aa206cc149eaddfcfe21e
vvs09 <137658234+vvs09@users.noreply.github.com>
2025-08-09 08:30:08 -0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import RailViharAdditionstimator from '../jacksonville_addition_estimator_ui.jsx';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RailViharAdditionEstimator />
+  </React.StrictMode>
+);
